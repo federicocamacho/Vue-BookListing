15bc111ebde75a853af6dba00b8981a3a7238b66
Federico Camacho <diego.camacho@yuxiglobal.com>
2021-04-13 11:08:53 -0500

    Module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,16 +2,17 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.title">
-        {{ book.title}}: {{ book.author }}
-      </li>
+      <book-item v-for="book in books" :key="book.title"></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+  import BookItem from './BookItem';
+
   export default {
     name: 'BookList',
+    components: { BookItem },
     data() {
       return {
         title: 'All Books',
@@ -33,10 +34,5 @@
   ul {
     list-style-type: none;
     padding: 0;
-  }
-
-  li {
-    display: block;
-    margin: 0 10px;
   }
 </style>
